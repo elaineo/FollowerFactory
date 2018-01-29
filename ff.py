a5a4e4b0e231354d12c9822020b614d38ab9b0fe
Elaine Ou <elaine.ou@gmail.com>
2018-01-29 15:14:41 -0800

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,40 @@
+from TwitterAPI import TwitterAPI
+import logging
+import json
+import time
+
+"""
+Fill in your keys before running. https://apps.twitter.com/
+"""
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+
+SCREEN_NAME = 'rogerkver'
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    bot = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    cursor = -1
+    count = 0
+
+    while True:
+        if not cursor:
+            break
+        fans_raw = bot.request('followers/list', {'screen_name': SCREEN_NAME, 'count': 200, 'cursor':cursor, 'skip_status':True, 'include_user_entities': False})
+        fans_clean = json.loads(fans_raw.response.text)
+        cursor = fans_clean['next_cursor']
+        logging.info(cursor)
+        fans = [f['created_at'] for f in fans_clean['users']]
+
+        with open('data.csv', 'a+') as f:
+            for fans in fans_clean['users']:
+                f.write("%s,%s \n" % (fans['id'], fans['created_at']) )
+
+        count += 200
+        logging.info("count: %s" % count)
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
